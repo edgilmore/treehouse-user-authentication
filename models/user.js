2d4657d0b53c7306e90edf64b2bd1ca1c9a99497
Ed Gilmore <edgilmore@live.com>
2017-05-08 11:03:26 -0700

    added support for hashing user passwords

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -22,6 +22,18 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+// hash the password before storing the record
+UserSchema.pre('save', function (next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    return next();
+  });
 });
 
 module.exports = mongoose.model('User', UserSchema);
